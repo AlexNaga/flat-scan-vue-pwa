f471050f36f69ff2651eb18c7da08d3d2650e318
Alex <alex.naga@postnord.com>
2020-05-17 00:46:44 +0200

    Fetch data and render it in cards

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="card-list">
     <div class="container is-fluid">
-      <Card
-        title="An example title"
-        rent="1337"
-        area="42"
-        mapUrl="A map URL"
-        imgUrl="https://bulma.io/images/placeholders/1280x960.png"
-        dateUpdated="asd 420"
-      />
+      <ul v-for="service in services" :key="service.id">
+        <li v-for="flat in service.flats" :key="flat">
+          <Card
+            :title="flat"
+            :rent="1337"
+            area="42"
+            mapUrl="A map URL"
+            imgUrl="https://bulma.io/images/placeholders/1280x960.png"
+            dateUpdated="asd 420"
+          />
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -21,10 +25,35 @@ export default {
   components: {
     Card,
   },
-};
+  data() {
+    return {
+      services: {},
+    };
+  },
+  beforeMount() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      // TODO: How to add envs to vue?
+      const url = 'https://k8b2vri9ga.execute-api.eu-north-1.amazonaws.com/test/flats';
+      const apiKey = 'Yg0SAzdhgV1jirIXPDy941lvR4pW1AXB8iccJGjd';
+      const options = {
+        headers: { 'x-api-key': apiKey },
+      };
 
-// const url = '';
-// const getData = () => {};
+      // this.loading = true;
+
+      const res = await fetch(url, options);
+      const data = await res.json();
+
+      // this.loading = false;
+      console.log(data);
+
+      this.services = data ? data : [];
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
