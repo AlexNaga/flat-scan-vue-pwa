1562becebe358877d0912bbfdf0f6d1402d44560
Alex <alex.naga@postnord.com>
2020-05-16 16:22:20 +0200

    Create components

---
+++ src/components/CardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-list">
+    <div class="container is-fluid">
+      <Card />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'CardList',
+  props: {
+    Card,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <img alt="flatScan logo" src="../assets/img/bunny.png" />
+
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ titleMsg }}</h1>
+          <h1 class="subtitle">{{ subTitleMsg }}</h1>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    titleMsg: String,
+    subTitleMsg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
