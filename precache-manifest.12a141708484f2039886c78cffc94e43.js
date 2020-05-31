60b178f0ab629c986c3155e339621211690ea38e
AlexNaga <alex.naga@hotmail.com>
2020-05-31 17:05:12 +0000

    Deploying to gh-pages from  @ 85661e6ef1ce11c9d4b54a34fb28f33c76451dcb 🚀

---
+++ precache-manifest.12a141708484f2039886c78cffc94e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729afd57452fa495ff30",
+    "url": "/flat-scan-vue-pwa/css/app.657ab6ed.css"
+  },
+  {
+    "revision": "b121011bb23b9c57eeb7",
+    "url": "/flat-scan-vue-pwa/css/chunk-vendors.9a091c43.css"
+  },
+  {
+    "revision": "7a454b43faf39fc2ca75fb5d96f2a7b3",
+    "url": "/flat-scan-vue-pwa/img/bunny.7a454b43.png"
+  },
+  {
+    "revision": "08912457f559a8617215ddf7b1282351",
+    "url": "/flat-scan-vue-pwa/index.html"
+  },
+  {
+    "revision": "729afd57452fa495ff30",
+    "url": "/flat-scan-vue-pwa/js/app-legacy.012b85b6.js"
+  },
+  {
+    "revision": "b121011bb23b9c57eeb7",
+    "url": "/flat-scan-vue-pwa/js/chunk-vendors-legacy.6cae378c.js"
+  },
+  {
+    "revision": "d88007ae6469f9f550175ca31fd5bd2a",
+    "url": "/flat-scan-vue-pwa/manifest.json"
+  }
+]);
---
+++ precache-manifest.c5d888147e5e7156b0b472153675e168.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5628c58def3ff3f38945",
+    "url": "/flat-scan-vue-pwa/css/app.657ab6ed.css"
+  },
+  {
+    "revision": "30ac8772d26e71dcda71",
+    "url": "/flat-scan-vue-pwa/css/chunk-vendors.9a091c43.css"
+  },
+  {
+    "revision": "7a454b43faf39fc2ca75fb5d96f2a7b3",
+    "url": "/flat-scan-vue-pwa/img/bunny.7a454b43.png"
+  },
+  {
+    "revision": "b467c478f862c389101d755960c348b3",
+    "url": "/flat-scan-vue-pwa/index.html"
+  },
+  {
+    "revision": "5628c58def3ff3f38945",
+    "url": "/flat-scan-vue-pwa/js/app.7d29d99e.js"
+  },
+  {
+    "revision": "30ac8772d26e71dcda71",
+    "url": "/flat-scan-vue-pwa/js/chunk-vendors.726b601a.js"
+  },
+  {
+    "revision": "d88007ae6469f9f550175ca31fd5bd2a",
+    "url": "/flat-scan-vue-pwa/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/flat-scan-vue-pwa/robots.txt"
+  }
+]);
