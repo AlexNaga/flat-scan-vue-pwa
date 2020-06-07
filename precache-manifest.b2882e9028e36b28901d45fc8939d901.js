32e5c86b9ff17533834288ddc30fa1f4562ce114
AlexNaga <alex.naga@hotmail.com>
2020-06-07 15:01:33 +0000

    Deploying to gh-pages from  @ a02e140ca40e9308dcc68a00137474e0114feb54 🚀

---
+++ precache-manifest.b2882e9028e36b28901d45fc8939d901.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a090d5f080c8474e0d7c",
+    "url": "/flat-scan-vue-pwa/css/app.d0dae2b0.css"
+  },
+  {
+    "revision": "829cbfdce1794fadb80d",
+    "url": "/flat-scan-vue-pwa/css/chunk-vendors.33eb119a.css"
+  },
+  {
+    "revision": "7a454b43faf39fc2ca75fb5d96f2a7b3",
+    "url": "/flat-scan-vue-pwa/img/bunny.7a454b43.png"
+  },
+  {
+    "revision": "07d5372dbd07cc919d369fd5f1fa1ae9",
+    "url": "/flat-scan-vue-pwa/flat-scan-vue-pwa/img/soft-wallpaper.png"
+  },
+  {
+    "revision": "36deda6bd30d612c129c1875b9d9049c",
+    "url": "/flat-scan-vue-pwa/index.html"
+  },
+  {
+    "revision": "a090d5f080c8474e0d7c",
+    "url": "/flat-scan-vue-pwa/js/app.f95f42b8.js"
+  },
+  {
+    "revision": "829cbfdce1794fadb80d",
+    "url": "/flat-scan-vue-pwa/js/chunk-vendors.182aa660.js"
+  },
+  {
+    "revision": "5c50d8cefa97218b20bc17cf0a98923a",
+    "url": "/flat-scan-vue-pwa/manifest.json"
+  },
+  {
+    "revision": "d88007ae6469f9f550175ca31fd5bd2a",
+    "url": "/flat-scan-vue-pwa/public/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/flat-scan-vue-pwa/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/flat-scan-vue-pwa/precache-manifest.f5fc4c23d8e5aef4d1d0d949a8265a0a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/flat-scan-vue-pwa/precache-manifest.b2882e9028e36b28901d45fc8939d901.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // TODO: Add Web Push
 
