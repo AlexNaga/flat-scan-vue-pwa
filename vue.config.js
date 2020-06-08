4fe3463e5d67d8cf87565c78c2ab3e9ed988cc3d
Alex <alex.naga@postnord.com>
2020-06-08 08:27:06 +0200

    Generate manifest instead of static

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,25 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/flat-scan-vue-pwa/' : '/',
   pwa: {
     name: 'flatScan',
-    themeColor: '#00aae7',
+    themeColor: '#FFF9E8',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    manifestPath: 'public/manifest.json',
+
+    // manifestPath: 'public/manifest.json',
+    manifestPath: 'manifest.json',
+    manifestOptions: {
+      start_url: './',
+      display: 'standalone',
+    },
+
+    iconPaths: {
+      // favicon16: 'img/icons/favicon-16x16.png',
+      // favicon32: 'img/icons/favicon-32x32.png',
+      // maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/icon-144x144.png',
+      appleTouchIcon: 'img/icons/icon-152x152.png',
+    },
 
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
