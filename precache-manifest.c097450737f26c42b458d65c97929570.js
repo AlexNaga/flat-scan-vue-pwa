5fdcbc416e9a8247c8dd1557ce3a8e87c0847395
AlexNaga <alex.naga@hotmail.com>
2020-06-08 07:17:40 +0000

    Deploying to gh-pages from  @ 2116c9976d86462130e983eb357e009e545290e6 🚀

---
+++ precache-manifest.c097450737f26c42b458d65c97929570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f1dfc18ecf98b7b5f6",
+    "url": "/flat-scan-vue-pwa/css/app.c2ffc3d7.css"
+  },
+  {
+    "revision": "9f7966fc24b3cd173025",
+    "url": "/flat-scan-vue-pwa/css/chunk-vendors.33eb119a.css"
+  },
+  {
+    "revision": "f2e901077ce82fae2ea4867733bc27ef",
+    "url": "/flat-scan-vue-pwa/img/bunny.f2e90107.png"
+  },
+  {
+    "revision": "ad7e86abbce647dc8dc1562dd7c2dfea",
+    "url": "/flat-scan-vue-pwa/index.html"
+  },
+  {
+    "revision": "50f1dfc18ecf98b7b5f6",
+    "url": "/flat-scan-vue-pwa/js/app-legacy.3fbb24ee.js"
+  },
+  {
+    "revision": "9f7966fc24b3cd173025",
+    "url": "/flat-scan-vue-pwa/js/chunk-vendors-legacy.bed455a3.js"
+  },
+  {
+    "revision": "41ac1eff302d0b5e50b2aed9813e1a58",
+    "url": "/flat-scan-vue-pwa/manifest.json"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/flat-scan-vue-pwa/precache-manifest.2b5e7c60b65f380af4467666632bb911.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/flat-scan-vue-pwa/precache-manifest.f87a4c4f26a5b5a0c9d75306ea71aa0e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // TODO: Add Web Push
 
