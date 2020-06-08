c6997d9337067d86ee64238b3926843880cfb7d6
AlexNaga <alex.naga@hotmail.com>
2020-06-08 06:07:14 +0000

    Deploying to gh-pages from  @ 60265216e28ad606208dfd216ffdaf49afdb2361 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/flat-scan-vue-pwa/precache-manifest.a1af97efb7503a8c5f29b674f5f6fc61.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/flat-scan-vue-pwa/precache-manifest.c5f70d16acc6c399efcb68498143221d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // TODO: Add Web Push
 
